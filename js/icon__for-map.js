93469d190650ef7359c1878229219bddba9365da
Natalia Yorkina <yorkinan@gmail.com>
2015-06-21 15:27:53 +0300

    add changes for better works

--- js/icon__for-map.js
+++ js/icon__for-map.js
@@ -1,1 +1,16 @@
-ymaps.ready(function(){var o;o=new ymaps.Map("footer__map--y",{center:[59.938554,30.32248],zoom:[15],controls:[]}),o.behaviors.disable("scrollZoom"),o.controls.add("zoomControl"),myPlacemark=new ymaps.Placemark([59.938563,30.3216],{hintContent:"Жмакабельно",balloonContent:""},{iconLayout:"default#image",iconImageHref:"../src/img/svg/map-marker.svg",iconImageSize:[40,40],iconImageOffset:[0,0]}),o.geoObjects.add(myPlacemark)});
+ymaps.ready(function() {
+    var o;
+    o = new ymaps.Map("footer__map--y", {
+        center: [59.938554, 30.32248],
+        zoom: [15],
+        controls: []
+    }), o.behaviors.disable("scrollZoom"), o.controls.add("zoomControl"), myPlacemark = new ymaps.Placemark([59.938563, 30.3216], {
+        hintContent: "Жмакабельно",
+        balloonContent: ""
+    }, {
+        iconLayout: "default#image",
+        iconImageHref: "./img/svg/map-marker.svg",
+        iconImageSize: [40, 40],
+        iconImageOffset: [0, 0]
+    }), o.geoObjects.add(myPlacemark)
+});
