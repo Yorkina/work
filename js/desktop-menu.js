dca18e68bd5dd3b03cb6a00a5bab8daca4437358
Natalia Yorkina <yorkinan@gmail.com>
2015-06-21 15:17:23 +0300

    debug js

--- js/desktop-menu.js
+++ js/desktop-menu.js
@@ -1,1 +1,30 @@
-!function(){for(var e=document.querySelectorAll('[name^="#nav"]'),t=.4,n=0;n<e.length;n++)e[n].addEventListener("click",function(e){function n(e){null===start&&(start=e);var o=e-start,r=position<0?Math.max(w-o/t,a+position):Math.min(a+o/t,a+position);window.scrollTo(0,r),r!=a+position&&requestAnimationFrame(n)}e.preventDefault();var o=document.querySelector(".header__btn-menu"),r=document.querySelector(".header__head-line");o.classList.remove("header__btn-menu--closed"),r.classList.remove("header__head-line--menu-style");var a=window.pageYOffset,i=this.href.replace(/[^#]*(.*)/,"$1");position=document.querySelector(i).getBoundingClientRect().top,start=null,requestAnimationFrame(n)},!1)}();
+;
+(function() {
+var linkNav = document.querySelectorAll('[href^="#nav"]'),
+    V = 0.4;
+for (var i = 0; i < linkNav.length; i++) {
+  linkNav[i].addEventListener('click', function(e) {
+    e.preventDefault();
+    var btnMenu = document.querySelector(".header__btn-menu");
+    var headerHeadline = document.querySelector(".header__head-line");
+    btnMenu.classList.remove("header__btn-menu--closed");
+    headerHeadline.classList.remove("header__head-line--menu-style");
+    var scroll = window.pageYOffset,
+        hash = this.href.replace(/[^#]*(.*)/, '$1');
+        position = document.querySelector(hash).getBoundingClientRect().top,
+        start = null;
+    requestAnimationFrame(step);
+    function step(time) {
+      if (start === null) start = time;
+      var progress = time - start,
+          roll = (position < 0 ? Math.max(w - progress/V, scroll + position) : Math.min(scroll + progress/V, scroll + position));
+      window.scrollTo(0,roll);
+      if (roll != scroll + position) {
+        requestAnimationFrame(step)
+      } else {
+        location.hash = hash;
+      }
+    }
+  }, false);
+}
+})();
